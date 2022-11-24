c9fdf87dcb6774eeed5d263648aaa024d99e6855
IGOR <sahmen@mail.ru>
2022-11-24 23:08:24 +0300

    DZ 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int p = n.ToString().Length;
+
+if (p < 3 || p > 3)
+Console.WriteLine("Вы ввели не трехзначное число");
+
+else
+{
+int n1 = n % 100;
+int n2 = n1;
+int n3 = n2 / 10;
+
+Console.WriteLine(n3);
+}
