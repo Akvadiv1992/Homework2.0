e1444458c6a45d8260053e53696a7a3a3bb0edf2
IGOR <sahmen@mail.ru>
2022-11-29 22:05:48 +0300

    DZ 2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 0) n = n * -1;
+
+if (n < 99)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+while (n > 1000)
+    n = n / 10;
+    n = n % 10;
+Console.WriteLine(n);}
